d8a2a5817a6ba74133be4ce3f483d92fb73fa2d9
sebasferruiz <36142019+sebasferruiz@users.noreply.github.com>
2018-07-31 15:33:17 -0400

    Update final_ar_tag_reader.py

--- Documents/final_1_0/final_ar_tag_reader.py
+++ Documents/final_1_0/final_ar_tag_reader.py
@@ -1,1 +1,15 @@
+from ar_track_alvar_msgs.msg import AlvarMarkers
+
 class arTagReader():
+  def __init__(self):
+        rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.callback, queue_size = 10)
+
+    def callback(self,marker):
+        if len(marker.markers) > 0:
+            if marker.markers[0].pose.id
+                return marker.markers[0].id
+
+if __name__ == "__main__":
+    rospy.init_node("ar_switch", anonymous = True)
+    node = ar_switch()
+    rospy.spin()
