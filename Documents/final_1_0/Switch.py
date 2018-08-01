81151d4ef5022632fda5963a3fea8a1210659c1c
Ed9ar <a01339939@itesm.mx>
2018-08-01 10:08:15 -0400

    Create Switch.py

---
+++ Documents/final_1_0/Switch.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+import rospy
+from ar_track_alvar_msgs.msg import AlvarMarkers
+import time
+import Right
+import Left
+
+
+class ar_switch():
+    def __init__(self):
+        rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.callback, queue_size = 1)
+
+    def callback(self,marker):
+        if len(marker.markers) > 0:
+            if marker.markers[0].id == 1:
+         	    Right.Follow_Wall()
+
+            if marker.markers[0].id == 4:
+                Left.Follow_Wall()
+                   
+
+if __name__ == "__main__":
+    rospy.init_node("ar_switch", anonymous = True)
+    node =ar_switch()
+rospy.spin()
