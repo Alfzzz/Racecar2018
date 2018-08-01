7fd483b01cb8a73a222fbbeeb4d987e94f3980eb
Ed9ar <a01339939@itesm.mx>
2018-08-01 13:58:21 -0400

    Update Switch.py

--- Documents/final_1_0/Switch.py
+++ Documents/final_1_0/Switch.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 import rospy
-#from sensor_msgs.msg import Image
 from ar_track_alvar_msgs.msg import AlvarMarkers
 import os
 import time
@@ -12,18 +11,22 @@ import left
 class ar_switch():
     def __init__(self):
         rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.callback, queue_size = 1)
-        #rospy.Subscriber("/camera/rgb/image_rect_color", Image, self.callback,  queue_size = 1)
+        self.master = 0
 
     def callback(self,marker):
-        if len(marker.markers) > 0:
-            if marker.markers[0].id == 1:
-         	right.right()
+        if (len(marker.markers) > 0) and:
+            if marker.markers.id != None:
+                print(marker.markers[0].id)
+                if marker.markers[0].id == 1:
+                    right.right()
 
-            if marker.markers[0].id == 4:
-                left.left()
-                   
+
+                elif marker.markers[0].id == 4:
+                    left.left()
+                else:
+                    pass
 
 if __name__ == "__main__":
     rospy.init_node("ar_switch", anonymous = True)
     node =ar_switch()
-    rospy.spin()
+rospy.spin()
