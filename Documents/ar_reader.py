d6f8f5c04ba02b40b8e32db77d7720cf796ba984
sebasferruiz <36142019+sebasferruiz@users.noreply.github.com>
2018-08-02 21:49:23 +0000

    Update ar_reader.py

--- Documents/ar_reader.py
+++ Documents/ar_reader.py
@@ -6,33 +6,33 @@ from ar_track_alvar_msgs.msg import AlvarMarkers
 AR_TOPIC = "/ar_pose_marker"
 flag = None
 last_ar = None
+minimum = None
 def ar_callback(ar_markers):
     global flag
     global last_ar
-    #for marker in ar_markers.markers:
-        #ar_id = marker.id
-        #marker.pose.pose.position.y
-        #last_id = ar_id
+    global minimum
+
     if len(ar_markers.markers) > 1:
         for i in range(1, len(ar_markers.markers)):
             minimum = ar_markers.markers[0].id
-            if ar_markers.markers[i].pose.pose.position.z < minimum:
+            if ar_markers.markers[i].pose.pose.position.y > minimum:
                 minimum = ar_markers.markers[i].id
         flag = 1
-        last_ar = minimum
     elif len(ar_markers.markers) == 1:
         minimum = ar_markers.markers[0].id
         flag = 1
-        last_ar = minimum
     else:
         pass
 
 
     if flag == None:
-        minimum = 0
+        last_ar = 0
     else:
-        minimum = last_ar
-    print minimum
+	if minimum < 50:
+    	    last_ar = minimum
+	else:
+            pass
+    print last_ar
 
 if __name__ == "__main__":
     try:
