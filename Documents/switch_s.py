fecf7674170230924541cd088aed6ef183b25119
sebasferruiz <36142019+sebasferruiz@users.noreply.github.com>
2018-08-03 10:38:17 -0400

    Update switch_s.py

--- Documents/switch_s.py
+++ Documents/switch_s.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 import rospy
-#from sensor_msgs.msg import Image
 from ar_track_alvar_msgs.msg import AlvarMarkers
 import os
 import time
@@ -11,13 +10,19 @@ import numpy as np
 from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import LaserScan
 
+AR_TOPIC = "/ar_pose_marker"
+flag = None
+last_ar = None
+selminimum = None
 
-class ar_switch():
+class Switch():
     def __init__(self):
         rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.callback, queue_size = 1)
         #::::::::::::::::::::::::::::::::::::: SUBSCRIBERS :::::::::::::::::::::::::::::::::
-        rospy.Subscriber("ackermann_cmd_mux/output", AckermannDriveStamped,self.ackermann_cmd_input_callback)
+        rospy.Subscriber("ackermann_cmd_mux/output", AckermannDriveStamped,self.callback)
         rospy.Subscriber("/scan", LaserScan, self.laser_callback)
+        ar_sub = rospy.Subscriber(AR_TOPIC, AlvarMarkers, ar_callback, queue_size=10)
+            rospy.spin()
         #::::::::::::::::::::::::::::::::::::: PUBLISHERS ::::::::::::::::::::::::::::::::::
         self.cmd_pub = rospy.Publisher('/vesc/ackermann_cmd_mux/input/navigation', AckermannDriveStamped, queue_size = 10)
         self.maxSpeed = 1
@@ -25,78 +30,79 @@ class ar_switch():
         self.velCoeff = 1
         # self.futCon = 0
         self.prev_error = 0
-        self.averageL = 0
-        self.futureL = 0
+        self.average = 0
+        self.future = 0
         self.wall = 0
-        self.averageR = 0
-        self.futureR = 0
         self.idealDis = 0.5 # En el caso de L or R
         self.output = 0
-                #:::::::::::::::::::::::::::::::::::: LINE :::::::::::::::::::::::::::::::::::::::::
+        #:::::::::::::::::::::::::::::::::::: LINE :::::::::::::::::::::::::::::::::::::::::
         self.contours = 0
         self.current_time = time.time() #No estpy seguro pero nos puede ayudar en un futuro
         self.prev_time = 0
         self.state = 0
 
+    def ar_callback(ar_markers):
+        global flag
+        global last_ar
+        global minimum
+
+        if len(ar_markers.markers) > 1:
+            for i in range(1, len(ar_markers.markers)):
+                minimum = ar_markers.markers[0].id
+                if ar_markers.markers[i].pose.pose.position.y > minimum:
+                    minimum = ar_markers.markers[i].id
+            flag = 1
+        elif len(ar_markers.markers) == 1:
+            minimum = ar_markers.markers[0].id
+            flag = 1
+        else:
+            pass
+
+        if flag == None:
+            last_ar = 0
+        else:
+        if minimum < 50 and ar_markers.markers[i].pose.pose.position.y > 0.5:
+                last_ar = minimum
+        else:
+                pass
+        print last_ar
 
     def laser_callback(self,msg):
         
-        ranges = msg.ranges
-
-        #Right average
-        self.averageR = np.mean(ranges[180 : 340])
-        self.futureR = np.mean(ranges[340 : 480])
-        #Front average
-        self.wall = np.mean(ranges[480 : 600])
-        #Left average
-        self.futureL = np.mean(ranges[600: 740])
-        self.averageL = np.mean(ranges[740 : 900])
-        if self.state == 0:
-            print "following right"
-            self.PID(1.0, 1.2, 0.0, 0.4, 'Right')
-        if self.state == 1:
-            print "following left"
+        if last_ar == 19:
+            ranges = msg.ranges
+            #Left average
+            self. = np.mean(ranges[600: 740])
+            print("future = {}".format(self.future)) ####
+            self.average = np.mean(ranges[740 : 900])
+            print("average = {}".format(self.average))
+            #Front average
+            self.wall = np.mean(ranges[480 : 600])
             self.PID(1.0, 1.2, 0.0, 0.4, 'Left')
-        #3 possible wall followers, Left, Right, LR, Close Line
 
-
-    def callback(self,marker):
-        if len(marker.markers) > 0:
-            if marker.markers[0].id == 18 or marker.markers[0].id == 4:
-                self.state = 1
-            if marker.markers[0].id == 23 or marker.markers[0].id == 7:
-                self.state = 0
-
+        if last_ar == 18:
+            ranges = msg.ranges
+            #Right average
+            self.average = np.mean(ranges[180 : 340])
+            print("future = {}".format(self.future))
+            self.future = np.mean(ranges[340 : 480])
+            print("average = {}".format(self.average))
+            #Front average
+            self.wall = np.mean(ranges[480 : 600])
+            self.PID(1.0, 1.2, 0.0, 0.4, 'Right')
 
     def PID(self, maxSpeed, kp, ki, kd, mode):
-        dir = 1
-        if  mode == 'Right':
-            error = self.averageR - self.idealDis
-            dir = -1 
-        elif mode == 'Left':
-            error = self.averageL - self.idealDis
-            print(error)
-        elif mode == 'LR':
-            error = (((self.averageL + self.averageR) / 2) - self.idealDis)
-        elif mode == 'Close':
-            if self.averageR < self.averageL:
-                error = self.averageR - self.idealDis
-                dir = -1 
-            else:
-                error = self.averageL - self.idealDis
+        print("ENTRANDO A PID")
+        error = self.average - self.idealDis
+        dir = -1 
+        if self.future >= (2 * self.average):
+            self.futCon = - 0.1
+        elif self.future <= self.average:
+            self.futCon = 0.1
         else:
-            if len(self.contours) > 0:
-                M = cv2.moments(max(contours, key = cv2.contourArea))
-                cx = int(M['m10'] / M['m00'])
-            
-            elif ZeroDivisionError:
-                cx = 640
-            
-            error = (float(self.cx) - 640) / (640)
+            elf.futCon = 0
 
         self.maxSpeed = maxSpeed
-        # Improve and future
-        
         self.current_time = time.time()
 
         #P
@@ -110,37 +116,31 @@ class ar_switch():
 
         self.prev_error = error
         self.prev_time = self.current_time
-
-        '''
-        Probar con el nuevo angulo
-        if (self.futureL - self.averageL) >= 0.235:
-            self.futCon = - 0.1
-        elif (self.futureL - self.averageL) <= 0.185:
-            self.futCon = 0.1
-        '''
-
-        self.output = (prop + integ + deriv) * dir # + futCon
+        self.output = (prop + integ + deriv + self.futCon) * dir
 
         if abs(self.output) >= 0.34:
             self.velCoeff = 0.7
+
         elif abs(self.output) >= 0.25:
             self.velCoeff = 0.9
+
         else:
             self.velCoeff = 1
 
-        #print("P = {} I = {} D = {}".format(round(prop, 4), round(integ, 4), round(deriv, 4)))
-        #print("Angle = {}".format(self.output))
+        print("P = {} I = {} D = {}".format(round(prop, 4), round(integ, 4), round(deriv, 4)))
+        print("Angle = {}".format(self.output))
 
-        self.ackermann_cmd_input_callback(AckermannDriveStamped())
+        print("SALIENDO DE PID")
+        # self.ackermann_cmd_input_callback(AckermannDriveStamped())
 
     def ackermann_cmd_input_callback(self, msg):
         msg.drive.speed = self.maxSpeed * self.velCoeff
         msg.drive.steering_angle = self.output
-        msg.drive.steering_angle_velocity = 0.8
+        msg.drive.steering_angle_velocity = 1
         self.cmd_pub.publish(msg)
 
 
-if __name__ == "__main__":
-    rospy.init_node("ar_switch", anonymous = True)
-    node =ar_switch()
-    rospy.spin()
+    if __name__ == "__main__":
+        rospy.init_node("Switch")
+        node = Switch()
+        rospy.spin()
