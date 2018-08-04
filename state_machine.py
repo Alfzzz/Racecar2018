bcbffc1ff34d65ab074e4dcd4bf48c8e48930435
Alfzzz <39445479+Alfzzz@users.noreply.github.com>
2018-08-04 10:23:12 -0500

    Update state_machine.py

--- state_machine.py
+++ state_machine.py
@@ -5,42 +5,44 @@ from right import Right
 #from start import Start
 from enum import IntEnum
 from ar_track_alvar_msgs.msg import AlvarMarkers
+from ar_switch import Ar_Find
+import math
 
-class State_Machine:
-    def __init__(self):
-        self.ar_marker_sub = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.ar_callback, queue_size=1)
-	STATES = {-1:'MidWall',0:'LeftWall',1:'RightWall',2:"Start",3:"}
-	rospy.init_node("switch_final")
-        current_state = STATES['LeftWall']
-	rate = rospy.Rate(10)
-	left=Left()
-	right=Right()
-	#start=Start()
-	rospy.loginfo("Buscando")
+
+STATES = {-1:'MidWall',1:'LeftWall',4:'RightWall',2:"Start",3:"}
+	  
 		  
-    def ar_callback(self,ar_markers):
-
-        if len(ar_markers.markers) > 1:
-            markers_distances = [marker.pose.pose.position.z for marker in ar_markers.markers]
-            min_index = markers_distances.index(min(markers_distances))
-            self.AR_ID = ar_markers.markers[min_index].id
-        print self.AR_ID
-
-	while not rospy.is_shutdown():
-	    #if not starlight:
-            #CURRENTstate
-            if self.AR_ID == 1:
-		current_state = STATES["LeftWall"]
-	    elif self.AR_ID == 4:
-		current_state = STATES["RightWall]
-	if current_state == 0:
+def ar_state(id):
+    return STATES[id]
+		  
+def main():
+    left=Left()
+    right=Right()
+    #start=Start()
+    ar=Ar_Find()
+	  
+    rospy.init_node("State_Machine")
+    rate=rospy.Rate(10)
+    current_state="RightWall"
+    rospy.loginfo("Buscando")
+	  
+    while not rospy.is_shutdown():
+	#if not starlight:
+        #CURRENTstate
+	current_state="RightWall"
+        try:
+            current_state = ar_state(my_ar_find.id)
+        except:
+            pass
+	  
+	if current_state == "RightWall":
 	    print"Right"
 	    right.rightw()
-	elif current_state ==1:
+	    ar.scan()
+	elif current_state =="leftWall":
 	    print "Left"              
 	    left.leftw()
-if __name__ == "__main__":
-    try:
-        rospy.init_node("State_Machine")
-        node = State_Machine()
-        rospy.spin()
+	    start_light.look()
+	    ar.scan()
+        rate.sleep()
+main()	
