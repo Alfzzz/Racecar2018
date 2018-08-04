5aba1d14a3bbb37a9a72ce12415398d1c01a64c3
Alfzzz <39445479+Alfzzz@users.noreply.github.com>
2018-08-03 21:26:02 -0500

    Update state_machine.py

--- state_machine.py
+++ state_machine.py
@@ -1,53 +1,46 @@
-
-#! /usr/bin/env python
-
+#!/usr/bin/env python
 import rospy
+from left import Left
+from right import Right
+#from start import Start
+from enum import IntEnum
 from ar_track_alvar_msgs.msg import AlvarMarkers
-from ackermann_msgs.msg import AckermannDriveStamped
-import time
-import right
-from right import *
-import left
 
-flag=None
-current_wall = 0
-class ar_switch1():
+class State_Machine:
     def __init__(self):
-        rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.ar_callback, queue_size = 1)
-        #rospy.Subscriber("/ackermann_cmd_mux/output", AckermannDriveStamped, right.Follow_Wall_Right.ackermann_cmd_input_callback)
-        #self.cmd_pub = rospy.Publisher('/ackermann_cmd_mux/input/default', AckermannDriveStamped, queue_size = 10)
+        self.ar_marker_sub = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.ar_callback, queue_size=1)
+	STATES = {-1:'MidWall',0:'LeftWall',1:'RightWall',2:"Start",3:"}
+	rospy.init_node("switch_final")
+        current_state = STATES['LeftWall']
+	rate = rospy.Rate(10)
+	left=Left()
+	right=Right()
+	#start=Start()
+	rospy.loginfo("Buscando")
+		  
+    def ar_callback(self,ar_markers):
 
-	
-        #self.rins = right.Follow_Wall_Right() 
-        #self.lins = left.Follow_Wall_Left() 
-
-
-        self.minimum=0    
-    def ar_callback(self, ar_markers):
-    	
-        global minimum,flag,last_ar
         if len(ar_markers.markers) > 1:
-            for i in range(1, len(ar_markers.markers)):
-                minimum = ar_markers.markers[0].id
-                if ar_markers.markers[i].pose.pose.position.y > minimum:
-                    minimum = ar_markers.markers[i].id
-            flag = 1
-        elif len(ar_markers.markers) == 1:
-            minimum = ar_markers.markers[0].id
-            flag = 1
-        else:
-            pass
+            markers_distances = [marker.pose.pose.position.z for marker in ar_markers.markers]
+            min_index = markers_distances.index(min(markers_distances))
+            self.AR_ID = ar_markers.markers[min_index].id
+        print self.AR_ID
 
-        if flag == None:
-            last_ar = 0
-        else:
-            if minimum < 50:
-                last_ar = minimum
-            else:
-                pass
-        print (last_ar)
-
+	while not rospy.is_shutdown():
+	    #if not starlight:
+            #CURRENTstate
+            if self.AR_ID == 1:
+		current_state = STATES["LeftWall"]
+	    elif self.AR_ID == 4:
+		current_state = STATES["RightWall]
+	if current_state == 0:
+	    print"Right"
+	    right.rightw()
+	elif current_state ==1:
+	    print "Left"              
+	    left.leftw()
 if __name__ == "__main__":
-    rospy.init_node("ar_switch1", anonymous = True)
-    node = ar_switch1()
-    rospy.spin()
+    try:
+        rospy.init_node("State_Machine")
+        node = State_Machine()
+        rospy.spin()
